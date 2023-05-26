1f7e5156c17552441ab5f85198f84050e5e10ad0
Dheeraj Prasad <dheerajprasad72@gmail.com>
2023-05-26 14:35:40 +0530

    Delete Node-JS directory

--- Node-JS/5-mainfun.js
+++
@@ -1,6 +0,0 @@
-const sayHI = (name) => {
-    console.log(`Hello Iam ${name}`);          //its an Normal program  
- }
-
-
- module.exports = sayHI;   //since here we only have one property to share we can share it directly !
